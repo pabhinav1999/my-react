7cfbcf35b25afdca5ecea70c22c95077352d30cd
Abhinav Kumar Pulijala <abhinavkumar.pulij@prod.hclpnp.com>
2024-07-15 05:55:18 +0530

    ep6 part1 code commit

---
+++ akshay-saini-react/lectures/talk_is_cheap_show_the_code_ep4/src/components/Shimmer.js
@@ -0,0 +1,16 @@
+const ShimmerUI = () =>{
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+        </div>
+    )
+}
+
+export default ShimmerUI;
