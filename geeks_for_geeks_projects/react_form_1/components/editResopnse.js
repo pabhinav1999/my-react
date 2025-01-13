f8ef3ea3bc81e84ee7296766dd8b9307f7d46725
Abhinav <pabhinav1999@gmail.com>
2025-01-13 08:10:50 +0530

    Simple Form Submission with useState Usage

--- geeks_for_geeks_projects/react_form_1/components/editResopnse.js
+++ geeks_for_geeks_projects/react_form_1/components/editResopnse.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { useState } from "react"
+import UserForm from "./userform";
+
 
 const EditResponse = (props) => {
+
+  /* Not using edit response as of now , will develop in future */
   console.log(props);
+  console.log('is it rerenmdered')
+
+  const [loadOldData, setLoadOldData] = useState(false)
+ 
   return (
     <div className="editResponseMessageContainer">
       <span> Your response is succesfully submitted</span>
       <span>
         {" "}
-        <a>Click here to edit the response again</a>
+        <span onClick={() => {
+            setLoadOldData((value) => !value)
+        }}>Click here to edit the response again</span>
       </span>
     </div>
   );
