6304a17205cda5d53cceb20b1d37d35c141e09fc
Abhinav <pabhinav1999@gmail.com>
2025-01-29 09:24:42 +0530

    React Component Life cycles

--- akshay-saini-react/lectures/talk_is_cheap_show_the_code_ep4/src/components/About.js
+++ akshay-saini-react/lectures/talk_is_cheap_show_the_code_ep4/src/components/About.js
@@ -1,15 +1,39 @@
 import User from "./User";
+import React from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-    return (
+// const About = () => {
+//     return (
+//         <div className="aboutContainer" >
+//             <h1> This is about page</h1>
+//             <h1> Lets Introduce team members</h1>
+//             {/* <User name={'Abhinav'} location={'Hyderabad'} /> */}
+//             <UserClass name={'Vikas Khanna Class'} location={'Mumbai Class'}/>
+//         </div>
+//     )
+// }
+
+ class About extends React.Component {
+
+    constructor(props) {
+        console.log('constructor of parent called')
+        super(props)
+    }
+
+    componentDidMount() {
+        console.log('component mount of parent is called')
+    }
+    render() {
+    console.log('render of parent is called')
+    return(
         <div className="aboutContainer" >
-            <h1> This is about page</h1>
-            <h1> Lets Introduce team members</h1>
-            {/* <User name={'Abhinav'} location={'Hyderabad'} /> */}
-            <UserClass name={'Vikas Khanna Class'} location={'Mumbai Class'}/>
-        </div>
+        <h1> This is about page</h1>
+        <h1> Lets Introduce team members</h1>
+        <UserClass name={'Abhinav'} location={'Mumbai'}/>
+    </div>
     )
+
+    }
 }
 
 export default About;
--- akshay-saini-react/lectures/talk_is_cheap_show_the_code_ep4/src/components/UserClass.js
+++ akshay-saini-react/lectures/talk_is_cheap_show_the_code_ep4/src/components/UserClass.js
@@ -4,27 +4,61 @@ class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
+        console.log( this.props.name + 'constructor of child is called')
+
         this.state = {
-            count1: 0,
-            count2: 1
+            userInfo: {
+                name : 'Dummy name',
+                id: 123,
+                avatar_url: ''
+            }
         }
+
+        // this.state = {
+        //     count1: 0,
+        //     count2: 1
+        // }
+    }
+
+    async componentDidMount() {
+        console.log(this.props.name + 'component mount of child is called')
+
+        // apis are called in component Did Mount func
+        const API_URL = 'https://api.github.com/users/pabhinav1999'
+        const resp =  await fetch(API_URL)
+        const json = await resp.json()
+
+        this.setState({userInfo: json})
+        console.log(json)
+
+    }
+
+    componentDidUpdate() {
+        console.log('component updated called')
+    }
+
+    componentWillUnmount() {
+        console.log('Component Unmounted');
     }
 
     render() {
         console.log(this.props, 'from Class')
-        const { name, location } = this.props
-        const { count1, count2 } = this.state
+        console.log(this.props.name + 'render of child is called')
+
+        const { name, id, avatar_url } = this.state.userInfo
+        // const { count1, count2 } = this.state
         return (
             <div className="userContainer">
-                 <button onClick={() => {
+                 {/* <button onClick={() => {
                     this.setState( {
                         count1 : this.state.count1 + 1
                     })
-                 }}> Increase Count</button>
-                 <h1> Count1: {count1} </h1>
+                 }}> Increase Count</button> */}
+                 {/* <h1> Count1: {count1} </h1> */}
                  {/* <h1> Count2: {count2} </h1> */}
+                 <img src={avatar_url}></img>
                  <h3> { name }</h3>
-                 <h4> { location } </h4>
+                 <h4> { id } </h4>
                  <h5> Chef</h5>
             </div>
         )
